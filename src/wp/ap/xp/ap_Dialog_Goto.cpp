578b01b58e6017c48062652644d0e98447be1703
Hubert Figuière <hub@figuiere.net>
2023-03-22 19:45:15 -0400

    Issue 15 - xp: don't use namespace for g_strdup()
    - it's a macro in the latest glib
    
    https://gitlab.gnome.org/World/AbiWord/-/issues/15

--- src/wp/ap/xp/ap_Dialog_Goto.cpp
+++ src/wp/ap/xp/ap_Dialog_Goto.cpp
@@ -69,13 +69,12 @@ void AP_Dialog_Goto::_setupJumpTargets(void)
 {
     const XAP_StringSet * pSS = m_pApp->getStringSet();
 
-    s_pJumpTargets[0] = ::g_strdup(pSS->getValue (AP_STRING_ID_DLG_Goto_Target_Page));
-    s_pJumpTargets[1] = ::g_strdup(pSS->getValue (AP_STRING_ID_DLG_Goto_Target_Line));
-    s_pJumpTargets[2] = ::g_strdup(pSS->getValue (AP_STRING_ID_DLG_Goto_Target_Bookmark));
-    //s_pJumpTargets[2] = ::g_strdup(pSS->getValue (AP_STRING_ID_DLG_Goto_Target_Picture)); //TODO
-    s_pJumpTargets[3] = ::g_strdup(pSS->getValue (AP_STRING_ID_DLG_Goto_Target_XMLid));    
-    s_pJumpTargets[4] = ::g_strdup(pSS->getValue (AP_STRING_ID_DLG_Goto_Target_Annotation));    
-
+    s_pJumpTargets[0] = g_strdup(pSS->getValue (AP_STRING_ID_DLG_Goto_Target_Page));
+    s_pJumpTargets[1] = g_strdup(pSS->getValue (AP_STRING_ID_DLG_Goto_Target_Line));
+    s_pJumpTargets[2] = g_strdup(pSS->getValue (AP_STRING_ID_DLG_Goto_Target_Bookmark));
+    //s_pJumpTargets[2] = g_strdup(pSS->getValue (AP_STRING_ID_DLG_Goto_Target_Picture)); //TODO
+    s_pJumpTargets[3] = g_strdup(pSS->getValue (AP_STRING_ID_DLG_Goto_Target_XMLid));
+    s_pJumpTargets[4] = g_strdup(pSS->getValue (AP_STRING_ID_DLG_Goto_Target_Annotation));
 }
 
 const char ** AP_Dialog_Goto::getJumpTargets(void)
